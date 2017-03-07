4f1de5675188203ee657ca70d1f7c0e601816481
oscaroceguera <oscaroceguera@gmail.com>
2017-03-06 23:09:04 -0700

    Dropdatabase & preload catalogs

--- config/db.js
+++ config/db.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose')
-// const sectionTypes = require('../data/sectionTypes.json')
-// const Section = require('../models/section')
+
+// Models
+const Section = require('../models/section')
+const Service = require('../models/service')
+const {Solicitud} = require('../models/solicitud')
+
+// Static data
+const sectionTypes = require('../data/sectionTypes.json')
+const serviceTypes = require('../data/serviceTypes.json')
+const solicitudTypes = require('../data/solicitudTypes.json')
 
 mongoose.connect(global.config.database)
 
@@ -9,6 +17,15 @@ const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error'))
 
 db.once('open', () => {
+
+  // Drop datata base
+  mongoose.connection.db.dropDatabase();
+
+  // Load static data
+  Service.create(serviceTypes)
+  Section.create(sectionTypes)
+  Solicitud.create(solicitudTypes)
+
   console.log('Connection with database success')
 })
 
